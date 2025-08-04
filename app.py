b48666e83db3d3b7a8198e9e035c46b581a3df90
Hiep-cao-cov <dinh.caohiep@gmail.com>
2025-08-04 08:45:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import plotly.graph_objects as go
 import drawchat
 import io
-from EDA import load_and_extract_dataframes
+from eda import load_and_extract_dataframes
 
 # Constants
 REQUIRED_COLUMNS = {
